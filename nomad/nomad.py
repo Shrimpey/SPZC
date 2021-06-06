d96445184f1271e99a506d559e0b781b9399dadf
shrimpey <kabat97@gmail.com>
2021-06-06 19:17:30 +0200

    Added time measurement to nomad calls

--- nomad/nomad.py
+++ nomad/nomad.py
@@ -1,6 +1,10 @@
 from django.core.handlers.wsgi import WSGIRequest
-
 from nomad.tagger import Tagger
+
+# ---   Time measurement related    ---
+from timeit import default_timer as timer
+measure_time = True
+# --- End time measurement related  ---
 
 
 class NomadMiddleware:
@@ -9,6 +13,10 @@ class NomadMiddleware:
         self.tagger = Tagger(parameter_tag='__RaNmE__')
 
     def __call__(self, request: WSGIRequest):
+        # ---   Time measurement related    ---
+        if measure_time:
+            starttime = timer()
+        # --- End time measurement related  ---
         print(request.session.session_key)  # DEBUG
         print(request.method == 'GET')
         print(self.get_client_ip(request))
@@ -24,6 +32,11 @@ class NomadMiddleware:
         elif request.method in ['POST', 'PUT']:
             pass  # TODO Trzeba zamienić że przecież w poscie nie idzie html tylko json, czy coś innego
             # Może coś takiego, że derandomizujemy htmla i tworzymy słownik {randomizowane_id : id}
+
+        # ---   Time measurement related    ---
+        if measure_time:
+            print("Time taken for Nomad call:", str((timer() - starttime)*1000) + " ms")
+        # --- End time measurement related  ---
         return response
 
     @staticmethod
